069bea0fa3c7d18d8557b45e62578fadc3725498
Derakios <kiki56860@gmail.com>
2022-11-15 13:44:55 +0100

    Setup articles

---
+++ components/articles.js
@@ -0,0 +1,19 @@
+app.component('listearticles', {
+    template:
+    `
+    <div class="divArticles">
+        <h3>Test component articles articles</h3>
+    </div>
+    `,
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        
+    },
+    props: {
+
+    }
+})
